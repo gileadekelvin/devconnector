f14ada1dccff891ea612b19ef076ea61c9173b71
gileadekelvin <gileadekelvin@gmail.com>
2018-10-10 18:26:21 -0300

    Configuração inicial da API Adiciona configuração para a conexão com o mongodb Cria arquivos para as rotas

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://devuser:devuser123@ds229373.mlab.com:29373/devconnector'
+}
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
+const app = express();
+
+// DB config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+    .connect(db)
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => console.log(err));
+
+app.get('/api', (req, res) => res.send('API works!!!'));
+
+// Use Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () =>  console.log(`API running on port ${port}`));
