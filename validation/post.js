0553fd6f26886f20d74f6b18e5fa8683197c7161
gileadekelvin <gileadekelvin@gmail.com>
2018-10-13 18:04:42 -0300

    Adiciona rotas para criar um Post

---
+++ validation/post.js
@@ -0,0 +1,21 @@
+const validator = require("validator");
+const isEmpty = require("./is-empty");
+
+module.exports = function validatePostInput(data) {
+  let errors = {};
+
+  data.text = !isEmpty(data.text) ? data.text : "";
+
+  if (!validator.isLength(data.text, {min: 10, max: 1000})) {
+    errors.text = "Post must be 10 and 1000 characters";
+  }
+
+  if (validator.isEmpty(data.text)) {
+    errors.text = "Text field is required";
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
