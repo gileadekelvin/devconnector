cc576e786c2fbc3805324245460f7ec27eb59871
gileadekelvin <gileadekelvin@gmail.com>
2018-10-19 19:21:01 -0300

    Adiciona forms de login e registro

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,89 @@
+import React, { Component } from "react";
+import classnames from "classnames";
+
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      errors: {}
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const user = {
+      email: this.state.email,
+      password: this.state.password
+    };
+
+    console.log(user);
+  }
+
+  render() {
+    const { errors } = this.state;
+
+    return (
+      <div className="login">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Log In</h1>
+              <p className="lead text-center">
+                Sign in to your DevConnector account
+              </p>
+              <form noValidate onSubmit={this.onSubmit}>
+                <div className="form-group">
+                  <input
+                    type="email"
+                    className={classnames("form-control form-control-lg", {
+                      "is-invalid": errors.email
+                    })}
+                    placeholder="Email Address"
+                    name="email"
+                    value={this.state.email}
+                    onChange={this.onChange}
+                  />
+                  {errors.email && (
+                    <div className="invalid-feedback">{errors.name}</div>
+                  )}
+                </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className={classnames("form-control form-control-lg", {
+                      "is-invalid": errors.password
+                    })}
+                    placeholder="Password"
+                    name="password"
+                    value={this.state.password}
+                    onChange={this.onChange}
+                  />
+                  {errors.password && (
+                    <div className="invalid-feedback">{errors.name}</div>
+                  )}
+                </div>
+                <input
+                  type="submit"
+                  className="btn btn-info btn-block mt-4"
+                  value="Submit"
+                />
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Login;
--- validation/register.js
+++ validation/register.js
@@ -25,21 +25,23 @@ module.exports = function validateRegisterInput(data) {
     errors.email = "Email is invalid";
   }
 
+  if (!validator.isLength(data.password, { min: 6, max: 30 })) {
+    errors.password = "Password must be at least 6 characters";
+  }
+
   if (validator.isEmpty(data.password)) {
     errors.password = "Password field is required";
   }
 
-  if (!validator.isLength(data.password, { min: 6, max: 30 })) {
-    errors.password = "Password must be at least 6 characters";
+  if (!validator.equals(data.password, data.password2)) {
+    errors.password2 = "Passwords must match";
   }
 
   if (validator.isEmpty(data.password2)) {
     errors.password2 = "Confirm Password field is required";
   }
 
-  if (!validator.equals(data.password, data.password2)) {
-    errors.password2 = "Passwords must match";
-  }
+
 
   return {
     errors,
